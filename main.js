c6e75259d56bb140d7bac8a7f3cacc67eb2ba2a8
abalun52 <abalun52@gmail.com>
2023-08-20 09:02:24 +0800

    餐廳資訊(網頁版)完成

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Swiper from 'swiper/bundle';
-
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Swiper from "swiper/bundle";
 
 //發表文章 tags 手機版用swiper
-const swiper = new Swiper('.tag-swiper', {
-    //避免使用者移動到
-    allowTouchMove: true,
-    spaceBetween: 16,
-    //一次呈現幾個.swiper-slide
-    slidesPerView: "auto",
+const swiper = new Swiper(".tag-swiper", {
+  //避免使用者移動到
+  allowTouchMove: true,
+  spaceBetween: 16,
+  //一次呈現幾個.swiper-slide
+  slidesPerView: "auto",
 
-    grabCursor: true,
+  grabCursor: true,
 
-    breakpoints: { // 斷點顯示設定
-      320: {  // 當裝置寬大於 320 
-        slidesPerView: 3, // 要看見幾頁內容 (可用小數點讓使用者知道可滑動)
-        spaceBetween: 16
-      },
-      768: {  // 當裝置寬大於 768
-        slidesPerView: 5,
-        spaceBetween: 16
-      },
-
+  breakpoints: {
+    // 斷點顯示設定
+    320: {
+      // 當裝置寬大於 320
+      slidesPerView: 3, // 要看見幾頁內容 (可用小數點讓使用者知道可滑動)
+      spaceBetween: 16,
     },
-
-
-  });
-
+    768: {
+      // 當裝置寬大於 768
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+  },
+});
